fea9e1bf0504fa328e9e2c2a810616e2ce84362b
david-again <oluseyi.ogunbanjo@gmail.com>
2020-09-05 17:13:03 +0000

    update project IDs and buckets

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "terraform-flask-288614-tfstate"
     prefix = "env/dev"
   }
 }
--- environments/prod/terraform.tfvars
+++ environments/prod/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="terraform-flask-288614"
